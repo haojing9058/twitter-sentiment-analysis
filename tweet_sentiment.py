16c3645dd0ebd4b439bc821469a747ecbfde8474
Jing Hao <haojing9058@gmail.com>
2018-04-16 14:11:25 -0700

    modifed tweet_sentiment.py to print the sentiment of each tweet in problem_2_submission.txt

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,21 +1,7 @@
 import sys
 import json
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
-
-def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
-
-
-afinnfile = open("AFINN-111.txt")
+afinnfile = open(sys.argv[1])
 scores = {} # initialize an empty dictionary
 for line in afinnfile:
     term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
@@ -23,16 +9,19 @@ for line in afinnfile:
 
 
 def get_sentiment_score(s):
-    terms = s.split()
+
     score = 0
+    terms = s.split()
     for term in terms:
         if term in scores:
-            score += scores['term']
+            score += scores[term]
     return score
 
 
+tweet_file = open(sys.argv[2])
 def get_tweets(file):
     for line in file.readlines():
+        line = line.encode('utf-8')
         tweet = json.loads(line)
         if 'text' in tweet:
             tweet_text = tweet['text']
@@ -40,8 +29,8 @@ def get_tweets(file):
         else:
             tweet_text = ''
             score = 0
-        print score # and save to a file
+        print score 
 
 
 if __name__ == '__main__':
-    main()
+    get_tweets(tweet_file)
